26c4d30a96240dc0e40b6d39fed61da108f1b8fa
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-09 17:14:49 -0300

    Search page responsiveness

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,14 +1,21 @@
 import './styles.css';
 import Navbar from 'components/Navbar';
+import SearchCard from 'components/SearchCard';
 
 const Search = () => {
-
-    return (
-        <>
-            <Navbar />
-            <h1>Página Search</h1>
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar />
+      <div className="page-container">
+        <div className="top-container">
+          <SearchCard />
+        </div>
+        <div className="bottom-container">
+          <SearchCard />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Search;
