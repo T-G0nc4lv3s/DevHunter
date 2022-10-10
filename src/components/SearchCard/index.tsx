955448b5171f8698b3704a207697ff3f86341736
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-09 23:57:14 -0300

    SearchCard useState and form events

--- src/components/SearchCard/index.tsx
+++ src/components/SearchCard/index.tsx
@@ -1,16 +1,40 @@
 import './styles.css';
+import {useState} from 'react';
+
+type FormData = {
+  profile: string;
+}
 
 const SearchCard = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    profile:''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.warn(formData);
+  }
+
   return (
     <div className="card-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-container">
           <label htmlFor="ctrl-search">Encontre um perfil no Github</label>
           <input
             type="text"
+            name="profile"
+            value={formData.profile}
             id="ctrl-search"
             placeholder="Usuário Github"
-            onChange={() => {}}
+            onChange={handleChange}
           />
           <input
             type="submit"
